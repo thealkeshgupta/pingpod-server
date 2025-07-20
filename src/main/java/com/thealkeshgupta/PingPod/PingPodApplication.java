975ffc0d5a190476673226e1f8ed72a82fea3fb0
Alkesh Gupta <thealkeshgupta@gmail.com>
2025-07-20 15:04:48 +0530

    Set up Spring Boot project

---
+++ src/main/java/com/thealkeshgupta/PingPod/PingPodApplication.java
@@ -0,0 +1,13 @@
+package com.thealkeshgupta.PingPod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingPodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingPodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thealkeshgupta/PingPod/PingPodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thealkeshgupta.PingPod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingPodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
